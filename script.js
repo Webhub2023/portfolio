947c2fcae0d38af2afbd0311d7cd3a7bb3f36390
nooralam <mdnooralam66266@gmail.com>
2024-11-08 02:20:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+ /* ==============  sticky navbar ================= */
+ let menuIcon = document.querySelector('#menu-icon');
+ let navbar  = document.querySelector('.navbar');
+
+ menuIcon.onclick = () => {
+   
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+ };
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+             navLinks.forEach(Links => {
+                Links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+             });
+        };
+
+    });  
+
+     /* ==============  sticky navbar ================= */
+     let header = document.querySelector('header');
+
+    header.classList.toggle('*sticky', window.scrollY > 100);
+
+      /* ==============  sticky navbar ================= */
+    menuIcon.classList.remove('square-xmark');
+    navbar.classList.remove('active');
+ };
+/* ==============  Scrollreveal================= */
+
+ScrollReveal({ 
+   reset: true,
+    distance: '80px',
+     duration: 2000,
+    delay: 200
+
+  });
+
+  ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+  ScrollReveal().reveal('.home-img, .services-container, .portfplio-box, .contact form', { origin: 'bottom' });
+  rollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+   rollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ 
+  /* ============== typed js================= */
+  const typed = new typed('.multiple-text',{
+  strings:['Frontend DEveloper', 'YouTuber', 'Bloger'],
+  typeSpeed: 100,
+ backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+ });
