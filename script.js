884954745b4f21faee4d815316b346575954c1f0
nooralam <mdnooralam66266@gmail.com>
2024-11-08 03:21:09 +0530

    second commit

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal({
     distance: '80px',
      duration: 2000,
     delay: 200
-
+   
   });
 
   ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
@@ -60,4 +60,7 @@ ScrollReveal({
  backSpeed: 100,
   backDelay: 1000,
   loop: true
+  
  });
+
+ console.log("hello world");
